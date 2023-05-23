59ea64463dc7833d0310c97777c7958c8306e90c
Devin Kennedy <67341568+devin-kennedy@users.noreply.github.com>
2023-05-23 08:52:09 -0400

    Logistic Map

--- differential.py
+++ differential.py
@@ -4,26 +4,53 @@ import math
 from functools import cache
 from time import thread_time
 from scipy.optimize import root
-dt = 0.1
-x0_pred = 1000
-x0_prey = 1000
+
+dt = 10 ** -3
+x0_pred = 100
+x0_prey = 100
 
 
 def main():
-    n = 50000
     # valsx = list(range(n))
     # valsy = [x(i) for i in range(n)]
     #
     # plt.plot(valsx, valsy)
     # plt.show()
-    preys, preds = x_backwards(n)
-    preys = list(preys)
-    preds = list(preds)
 
-    valsx = list(range(len(preys)))
 
-    plt.plot(valsx, preys)
-    plt.plot(valsx, preds)
+    # preys, preds = x_backwards(n)
+    # preys = list(preys)
+    # preds = list(preds)
+    #
+    # valsx = list(range(len(preys)))
+    #
+    # plt.plot(valsx, preys)
+    # plt.plot(valsx, preds)
+    # plt.show()
+
+    # i_s = 100
+    # r = 3.59
+    # x0 = 0.5
+    # valsx = list(range(i_s))
+    # plt.plot(valsx, logisticMap(x0, r, i_s))
+    # plt.xlabel("x")
+    # plt.ylabel("y")
+    # plt.title(f"r = {r}   x0 = {x0}")
+    # plt.show()
+
+    n = 15
+    prec = 0.01
+    maps = {}
+    for r in list(np.arange(2.0, 4.0, 0.01)):
+        map = logisticMap(0.3, r, 150)
+        maps[r] = map[-n:]
+
+    for r, map in list(maps.items()):
+        plt.scatter([r for _ in range(len(map))], map, c="blue", s=0.1)
+
+    plt.title(f"n = {n}, ")
+    plt.xlabel("r value")
+    plt.ylabel("")
     plt.show()
 
 
@@ -66,6 +93,19 @@ def x_backwards(n):
         preds[i] = sol.x[1]
 
     return preys, preds
+
+
+def logisticMap(x0, r, iters):
+    xs = [x0]
+
+    for i in range(1, iters):
+        xs.append(f_logmap(xs[i-1], r))
+
+    return xs
+
+
+def f_logmap(x, r):
+    return r * x * (1 - x)
 
 
 if __name__ == "__main__":
